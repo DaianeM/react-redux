fb7b2e9db25c36077ec12ff5b5cc84478aa24878
Daiane Matos <daiane@pluralsystems.com.br>
2021-10-25 16:28:03 -0300

    Estilizando home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from 'react';
+import { MdFlightTakeoff } from 'react-icons/md';
 
 import api from '../../services/api';
+
+import './home.css';
 
 export default function Home() {
   const [trips, setTrips] = useState([]);
@@ -18,7 +21,24 @@ export default function Home() {
 
   return(
     <div>
-      <h1>Home</h1>
+      <div className="box">
+        {trips.map(trip => (
+          <li key={trip.id}>
+            <img src={trip.image} alt={trip.title}/>
+            <strong>{trip.title}</strong>
+            <span>Status: {trip.status ? 'Disponível' : 'Indisponível'}</span>
+            <button 
+            type="button"
+            onClick={()=>{}}
+            >
+              <div>
+                <MdFlightTakeoff size={16} color="#FFF"/>
+              </div>
+              <span>Solicitar a reserva</span>
+            </button>
+          </li>
+        ))}
+      </div>
     </div>
   );
 }
